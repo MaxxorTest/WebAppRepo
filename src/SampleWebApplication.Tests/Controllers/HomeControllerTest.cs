b76acc6a1a110a56c88c68a419fef79e7cf8af92
MAXXOR\jason.bennett <jason.bennett@maxxor.com>
2016-10-18 18:07:35 +0200

    initial solution commit and push

---
+++ src/SampleWebApplication.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,49 @@
+﻿using System.Web.Mvc;
+using SampleWebApplication.Controllers;
+using Xunit;
+
+
+namespace SampleWebApplication.Tests.Controllers
+{
+    public class HomeControllerTest
+    {
+        [Fact]
+        public void Index()
+        {
+            // Arrange
+            var controller = new HomeController();
+
+            // Act
+            var result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public void About()
+        {
+            // Arrange
+            var controller = new HomeController();
+
+            // Act
+            var result = controller.About() as ViewResult;
+
+            // Assert
+            Assert.Equal("Your application description page.", result.ViewBag.Message);
+        }
+
+        [Fact]
+        public void Contact()
+        {
+            // Arrange
+            var controller = new HomeController();
+
+            // Act
+            var result = controller.Contact() as ViewResult;
+
+            // Assert
+            Assert.NotNull(result);
+        }
+    }
+}
