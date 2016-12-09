ac26f3209c411255631149ba01487fa4e450ec41
MAXXOR\jason.bennett <jason.bennett@maxxor.com>
2016-12-09 14:55:22 +0200

    added arithmetic class and 2 methods included unit tests

---
+++ src/SampleWebApplication/Classes/Arithmetic.cs
@@ -0,0 +1,15 @@
+namespace SampleWebApplication.Classes
+{
+    public class Arithmetic
+    {
+        public int Add2Numbers(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Multiply2Numbers(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
